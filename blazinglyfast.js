8334afa0207d717a4ba37921a59236e954e02f25
hans <hkmelaa@hotmail.com>
2024-08-31 22:12:52 +0200

    more

--- blazinglyfast.js
+++ blazinglyfast.js
@@ -1,70 +1,75 @@
 (() => {
-  console.log(document.currentScript.parentElement.tagName);
-
   const form = document.currentScript.parentElement;
   if (form.tagName !== "FORM") {
-    console.warn("not a form");
+    console.error("not a form");
     return;
   }
 
-  const _styleSheetKey = `ezpez-${Math.random().toString(36).substring(2)}`;
+  const _styleSheetKey = `ezpez-${Math.random()
+    .toString(36)
+    .substring(2)
+    .slice(-5)}`;
+
   const style = `
-  .${_styleSheetKey} {
-    font-family: sans-serif;
-  
-    section {
-      display: flex;
-      flex-direction: column;
-      gap: 0.5em;
-      max-width: 35ch;
+    .${_styleSheetKey} {
+
+      font-family: sans-serif;
+      color: #313131;
+    
+      section {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5em;
+        max-width: 35ch;
+      }
+    
+      input,button,textarea {
+        font: inherit;
+        color: inherit;
+      }
+    
+      & > button {
+        margin-top: 2em;
+      }
+    
+      [data-bind]:empty {
+        display: none;
+      }
     }
-  
-    input {
-      font: inherit;
+    .debug-${_styleSheetKey}{
+      position:fixed;
+      top: 0;
+      right: 0;
+      z-index: 0;
+      padding: 1em;
+      margin: .5em;
+      background-color: black;
+      color: white;
+      border: 1px solid gray;
     }
-  
-    & > button {
-      margin-top: 2em;
-    }
-  
-    [data-bind]:empty {
-      display: none;
-    }
-  }
   `;
 
-  const stylesheet = document.createElement("style");
-  stylesheet.textContent = style;
-  form.classList.add(_styleSheetKey);
-  form.append(stylesheet);
+  if (form.attributes["debug"] !== undefined) {
+    console.warn("Form debug enabled");
 
-  const inputs = [...form.elements].filter(({ name }) => name?.length);
-  const inputsNames = inputs.map(({ name }) => name);
+    const pre = document.createElement("pre");
+    pre.classList.add(`debug-${_styleSheetKey}`);
+    form.insertBefore(pre, document.currentScript);
 
-  const bindings = new Map();
-  [...form.querySelectorAll("[data-bind]")].forEach((node) => {
-    const key = node.dataset.bind;
-    if (!bindings.has(key)) {
-      bindings.set(key, []);
-    }
-    bindings.get(key).push(node);
-  });
-
-  const syncBindings = () => {
-    bindings.forEach((nodes) => {
-      nodes.forEach((node) => {
-        node.textContent = form.elements[node.dataset.bind]?.value || "";
-      });
+    form.addEventListener("dataupdate", ({ detail }) => {
+      pre.textContent = JSON.stringify(
+        {
+          keys: inputsNames,
+          data: prettyForm(form),
+        },
+        null,
+        2
+      );
     });
-  };
+  }
 
-  form.addEventListener("input", syncBindings);
-  form.addEventListener("change", syncBindings);
-  form.addEventListener("reset", () => setTimeout(syncBindings));
-  syncBindings();
-
-  const prettyForm = (form) => {
-    return [...new FormData(form).entries()].reduce((acc, [k, v]) => {
+  const prettyForm = (formNode) => {
+    return [...new FormData(formNode).entries()].reduce((acc, [k, v]) => {
       if (!v) return acc;
 
       let current = acc;
@@ -83,8 +88,42 @@
     }, {});
   };
 
+  const stylesheet = document.createElement("style");
+  stylesheet.textContent = style;
+  form.classList.add(_styleSheetKey);
+  form.append(stylesheet);
+
+  const inputs = [...form.elements].filter(({ name }) => name?.length);
+  const inputsNames = inputs.map(({ name }) => name);
+  const bindings = new Map();
+  [...form.querySelectorAll("[data-bind]")].forEach((node) => {
+    const key = node.dataset.bind;
+    if (!inputsNames.includes(key)) return;
+    if (!bindings.has(key)) bindings.set(key, []);
+    bindings.get(key).push(node);
+  });
+
+  const syncBindings = () => {
+    bindings.forEach((nodes) => {
+      nodes.forEach((node) => {
+        node.textContent = form.elements[node.dataset.bind]?.value || "";
+      });
+    });
+
+    const event = new CustomEvent("dataupdate", {
+      bubbles: true,
+      detail: prettyForm(form),
+    });
+
+    form.dispatchEvent(event);
+  };
+
+  form.addEventListener("input", syncBindings);
+  form.addEventListener("change", syncBindings);
+  form.addEventListener("reset", () => setTimeout(syncBindings));
+  syncBindings();
+
   form.addEventListener("submit", (ev) => {
     ev.preventDefault();
-    formResult.textContent = JSON.stringify(prettyForm(ev.target), null, 2);
   });
 })();
